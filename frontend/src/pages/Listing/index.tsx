cba6e8c494ad80fc5992e5b07da1b09e767516ec
Gabriel Comassetto <gabriel_hem@hotmail.com>
2022-03-12 20:43:54 -0300

    useState useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,27 @@
 import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { response } from "express";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/requests";
+
 function Listing() {
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+            .then(response => {
+                const data = response.data as MoviePage
+                setPageNumber(data.number)
+            });
+    }, []);
+
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
